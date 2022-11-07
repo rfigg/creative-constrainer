0e1aa46818fd29e8f014927c3155e48a2be3d6f9
Ryan Figg <ryan.figg@gmail.com>
2022-11-06 20:47:34 -0600

    add comments, fix case of single bad command line argument

--- cc.py
+++ cc.py
@@ -3,12 +3,15 @@ import sys
 
 print("Creative Constrainer")
 
+# pick random creative constraints
 instrument = random.choice(['electric guitar','acoustic guitar','piano','keys','beats','bass','drums'])
 tempo = random.randint(60,220)
 key = random.choice(['A','A#','B','C','C#','D','D#','E','F','F#','G','G#'])+" "+random.choice(['major','minor']) 
 
+# start of sentence to build with cmd line options
 message = 'Play '
 
+# build sentence with all options if none are specified
 if len(sys.argv)==1 or '--instrument' in sys.argv or '-i' in sys.argv:
 	message += 'the '+instrument+' '
 if len(sys.argv)==1 or '--key' in sys.argv or '-k' in sys.argv:
@@ -16,4 +19,8 @@ if len(sys.argv)==1 or '--key' in sys.argv or '-k' in sys.argv:
 if len(sys.argv)==1 or '--tempo' in sys.argv or '-t' in sys.argv:
 	message += 'at '+str(tempo)+' bpm.'
 
-print(message)
+# check for case of single invalid argument
+if message != 'Play ':
+	print(message)
+else:
+	print("Play the "+instrument+" in the key of "+key+" at "+str(tempo)+" bpm.")
