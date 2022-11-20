841783f6b892b4d59645494c72695246bdcbb687
Ryan Figg <ryan.figg@gmail.com>
2022-11-20 10:30:44 -0600

    Add time signature, modify sentence structure

--- cc.py
+++ cc.py
@@ -7,20 +7,24 @@ print("Creative Constrainer")
 instrument = random.choice(['electric guitar','acoustic guitar','piano','keys','beats','bass','drums'])
 tempo = random.randint(60,220)
 key = random.choice(['A','A#','B','C','C#','D','D#','E','F','F#','G','G#'])+" "+random.choice(['major','minor']) 
+signature = random.choice(['2/4','3/4','4/4','5/4','11/4','2/2','6/8','9/8','12/8','7/8'])
 
 # start of sentence to build with cmd line options
-message = 'Play '
+message = 'Play'
 
 # build sentence with all options if none are specified
 if len(sys.argv)==1 or '--instrument' in sys.argv or '-i' in sys.argv:
-	message += 'the '+instrument+' '
+	message += ' the '+instrument
 if len(sys.argv)==1 or '--key' in sys.argv or '-k' in sys.argv:
-	message += 'in the key of '+key+' '
+	message += ' in the key of '+key
 if len(sys.argv)==1 or '--tempo' in sys.argv or '-t' in sys.argv:
-	message += 'at '+str(tempo)+' bpm.'
+	message += ' at '+str(tempo)+' bpm'
+if len(sys.argv)==1 or '--signature' in sys.argv or '-s' in sys.argv:
+	message += ' in '+signature+' time'
+message += '.'
 
 # check for case of single invalid argument
-if message != 'Play ':
+if message != 'Play':
 	print(message)
 else:
-	print("Play the "+instrument+" in the key of "+key+" at "+str(tempo)+" bpm.")
+	print("Play the "+instrument+" in the key of "+key+" at "+str(tempo)+" bpm in "+signature+" time.")
